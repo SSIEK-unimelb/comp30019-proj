4ac2b4292047fa338f9797dbcd2f8f9f873879ce
zchaarim <142210119+zchaarim@users.noreply.github.com>
2024-09-15 03:00:25 +1000

    Added crossbow visuals

---
+++ Assets/PlayerAssets/Scripts/FireArrow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireArrow : MonoBehaviour
+{
+    [SerializeField] GameObject arrowPrefab;
+    [SerializeField] float arrowSpeed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fire()
+    {
+        if (arrowPrefab != null)
+        {
+            GameObject arrow = Instantiate(arrowPrefab, transform.position, transform.rotation * Quaternion.Euler(0, 90, 90));
+            Rigidbody rb = arrow.GetComponent<Rigidbody>();
+            rb.velocity = transform.forward * arrowSpeed;
+        }
+        else
+        {
+            Debug.LogError("Arrow prefab not assigned");
+        }
+    }
+}
--- Assets/PlayerAssets/Scripts/LeftMouseActivate.cs
+++ Assets/PlayerAssets/Scripts/LeftMouseActivate.cs
@@ -25,8 +25,26 @@ public class LeftMouseActivate : MonoBehaviour
         // also make sure that the object this is attached to is the active prefab arm
         if (itemSwitcher.currentItem.Equals(transform.parent.gameObject) && Input.GetKeyDown(KeyCode.Mouse0) && !holdScript.isHolding())
         {
-            // do action
-            animator.Play("LeftClick");
+            WeaponType weaponType = transform.GetComponent<WeaponType>();
+
+            if (weaponType == null) {
+                Debug.LogError("WeaponType is missing from weapon");
+            }
+
+            if (itemSwitcher == null || itemSwitcher.currentItem == null) {
+                Debug.LogError("ItemSwitcher or currentItem is null");
+            }
+
+            // do action - stabbing animation
+            if (weaponType.currentWeapon == WeaponType.Weapon.Knife) {
+                animator.Play("LeftClick");
+            }
+            else if (weaponType.currentWeapon == WeaponType.Weapon.Crossbow) {
+                // insert crossbow firing logic here
+                FireArrow arrowShooter = transform.GetComponent<FireArrow>();
+                arrowShooter.Fire();
+            }
         }
     }
+    
 }
