d6fa03ad72099cb297d7363a39be350b66ac1879
zchaarim <142210119+zchaarim@users.noreply.github.com>
2024-09-22 22:25:08 +1000

    Added ammo system to crossbow

--- Assets/PlayerAssets/Scripts/FireArrow.cs
+++ Assets/PlayerAssets/Scripts/FireArrow.cs
@@ -6,29 +6,47 @@ public class FireArrow : MonoBehaviour
 {
     [SerializeField] GameObject arrowPrefab;
     [SerializeField] float arrowSpeed = 100f;
+    [SerializeField] int startingAmmo = 3;
+    private GameObject ammoText;
+    private int currentAmmo;
     // Start is called before the first frame update
     void Start()
     {
+        currentAmmo = startingAmmo;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateAmmoUI();
     }
 
     public void Fire()
     {
         if (arrowPrefab != null)
         {
-            GameObject arrow = Instantiate(arrowPrefab, transform.position, transform.rotation * Quaternion.Euler(0, 90, 90));
-            Rigidbody rb = arrow.GetComponent<Rigidbody>();
-            rb.velocity = transform.forward * arrowSpeed;
+            if (currentAmmo > 0)
+            {
+                GameObject arrow = Instantiate(arrowPrefab, transform.position, transform.rotation * Quaternion.Euler(0, 90, 90));
+                Rigidbody rb = arrow.GetComponent<Rigidbody>();
+                rb.velocity = transform.forward * arrowSpeed;
+                currentAmmo--;
+            }
         }
         else
         {
             Debug.LogError("Arrow prefab not assigned");
         }
+    }
+
+    public void AddAmmo(int amount) 
+    {
+        currentAmmo += amount;
+    }
+
+    public void UpdateAmmoUI()
+    {
+
     }
 }
